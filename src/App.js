4ae56deee20ae8ccf3d453f6d0d8e81f4fef0d25
Hoang Phuc <hoangphuc91dtt@gmail.com>
2023-12-27 14:06:20 +0700

    hi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ProfileSuper from "./components/ProfileSuper/ProfileSuper";
+import Gallery from "./components/Gallery/Gallery";
+import PackingList from "./components/PackingList/PackingList";
+import List from "./components/List/List";
+import Signup from "./components/Signup/Signup";
+import StateComponent from "./components/State/StateComponent";
+import RedPoin from "./components/RedPoin/RedPoin";
+import Form1 from "./components/Form/Form1/Form1";
+import Form2 from "./components/Form/Form2/Form2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+      <div>
+        {" "}
+        <MyProfile />
+      </div>
+      <div>
+        <MyProduct />
+      </div>
+      <div>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+      </div>
+      <div>
+        <Game />
+      </div> */}
+      {/* <div>
+        <AppSandbox />
+      </div> */}
+      {/* <ProfileSuper />
+      <Gallery /> */}
+      {/* <PackingList /> */}
+      {/* <List /> */}
+      {/* <StateComponent /> */}
+      <Form2 />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Form1 />
+    </>
   );
 }
 
